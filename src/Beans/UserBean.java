1e971287b40c35bf4502bac7830f6ccbdf973e4e
Kristian <krissvor@gmail.com>
2017-05-03 14:15:27 +1000

    lagt tile mappe for controllere og beans. Post fra UserRegistration lager nå userBeans. Skjemavalidering er implementert, men deaktivert for skjemaet.

---
+++ src/Beans/UserBean.java
@@ -0,0 +1,7 @@
+package Beans;
+
+/**
+ * Created by kriss on 03-May-17.
+ */
+public class UserBean {
+}
---
+++ src/controllers/UserRegController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+/**
+ * Created by kriss on 03-May-17.
+ */
+public class UserRegController {
+}
